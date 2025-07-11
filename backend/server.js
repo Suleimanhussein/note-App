5e04284bf9417718ab329e62975fc1c546171f6d
Suleimanhussein <suleymaanxuseen24-gmailcom>
2025-07-12 02:27:20 +0300

    prepared by deployment

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const connectDB = require("./src/config/DB");
-// const connectDB = require("./src/config/DB")
+const path = require("path");
+
 const app = express();
-const cors = require("cors");
-app.use(cors());
+
+if (process.env.NODE_ENV !== "production") {
+  const cors = require("cors");
+  app.use(cors());
+}
+
 connectDB();
 app.use(express.json());
-const noteRoute = require("./src/routes/noteRoute");
 
+const noteRoute = require("./src/routes/noteRoute");
 app.use("/note", noteRoute);
 
+// Serve frontend build (for Vite)
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+});
 
 app.listen(5001, () => {
   console.log("Server is running on port: 5001");
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-
+const bBASE_URL=import.meta.env.MODE==='development' ? "http://localhost:5001" :"note"
 const api = axios.create({
     baseURL: "http://localhost:5001",
 });
