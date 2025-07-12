6430826760a713108534698abad86a2168af2d58
Suleimanhussein <suleymaanxuseen24-gmailcom>
2025-07-12 11:41:34 +0300

    prepared by deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,21 @@
 import axios from "axios";
 
-const bBASE_URL=import.meta.env.MODE==='development' ? "http://localhost:5001" :"note"
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5001"
+    : import.meta.env.VITE_API_URL; // Read from .env in Vercel
+
 const api = axios.create({
-    baseURL: "http://localhost:5001",
+  baseURL: BASE_URL,
 });
 
-
 export default api;
+
+// import axios from "axios";
+
+// const bBASE_URL=import.meta.env.MODE==='development' ? "http://localhost:5001" :"note"
+// const api = axios.create({
+//     baseURL: "http://localhost:5001",
+// });
+
+// export default api;
