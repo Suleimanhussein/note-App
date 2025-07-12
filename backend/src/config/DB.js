f034e36438db86209369537fcfcecc69db4244d5
Suleimanhussein <suleymaanxuseen24-gmailcom>
2025-07-12 03:00:16 +0300

    prepared by deployment

--- backend/src/config/DB.js
+++ backend/src/config/DB.js
@@ -2,12 +2,11 @@ const { default: mongoose } = require("mongoose");
 
 const connectDB = async () => {
   try {
-    mongoose.connect("mongodb://localhost:27017/note-App");
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("db connect successfully");
-  } catch (error) {
-      console.log("not conntect db",error)
+  } catch (err) {
+    console.error("db connection error:", err);
   }
 };
 
-
-module.exports=connectDB
+module.exports = connectDB;
